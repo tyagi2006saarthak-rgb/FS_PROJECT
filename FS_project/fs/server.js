c74b0bc33a387779c8678867b4e4b0f4f44de0da
tyagi2006saarthak-rgb <tyagi2006saarthak@gmail.com>
2026-04-23 21:13:17 +0530

    Update server.js

--- FS_project/fs/server.js
+++ FS_project/fs/server.js
@@ -7,8 +7,8 @@ const { URL } = require("url");
 const publicDir = path.join(__dirname, "public");
 
 const adminCredentials = {
-  userId: "sxxr","gourav",
-  password: "0307","gour"
+  userId: "sxxr",
+  password: "0307"
 };
 
 const validStatuses = new Set(["open", "inProgress", "resolved"]);
